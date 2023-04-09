847d1da058e48fa995140323bd2f0364dbf29c92
alex050395 <121769512+alex050395@users.noreply.github.com>
2023-04-09 20:41:06 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -8,7 +8,15 @@ def solution(x_success: int,
              x_cnt: int, 
              y_success: int, 
              y_cnt: int) -> bool:
-    # Измените код этой функции
-    # Это будет вашим решением
-    # Не меняйте название функции и её аргументы
-    return x_success/x_cnt - y_success/y_cnt > 0.015
+    alpha=0.02
+    p1 = x_success / x_cnt
+    p2 = y_success / y_cnt
+    p_combined = (x_success + y_success) / (x_cnt + y_cnt)
+    difference = p1 - p2
+ 
+    z_value = difference / np.sqrt(p_combined * (1 - p_combined) * (1 / x_success + 1 / y_success)) 
+    distr = stats.norm(0, 1)  
+    
+    p_value = (1 - distr.cdf(abs(z_value))) * 2 
+    
+    return p_value < alpha
