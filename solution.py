3c24794c94164b5da2f730bbd15a9da56c4b0404
alex050395 <121769512+alex050395@users.noreply.github.com>
2023-04-10 18:00:38 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import numpy as np
-from scipy import stats
+from statsmodels.stats.proportion import proportions_ztest
 
 chat_id = 5481533510 # Ваш chat ID, не меняйте название переменной
 
@@ -8,15 +8,7 @@ def solution(x_success: int,
              x_cnt: int, 
              y_success: int, 
              y_cnt: int) -> bool:
-    alpha=0.02
-    p1 = x_success / x_cnt
-    p2 = y_success / y_cnt
-    p_combined = (x_success + y_success) / (x_cnt + y_cnt)
-    difference = p1 - p2
- 
-    z_value = difference / np.sqrt(p_combined * (1 - p_combined) * (1 / x_success + 1 / y_success)) 
-    distr = stats.norm(0, 1)  
+    _, pval = proportions_ztest([x_success, y_success], [x_cnt, y_cnt], alternative='larger')
+    effect = (pval < 0.02)
     
-    p_value = (1 - distr.cdf(abs(z_value))) * 2 
-    
-    return p_value < alpha
+    return effect
